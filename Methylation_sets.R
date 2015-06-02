cfcda97a0602ea5721eb897d5aec30c35deafec4
Korinne Northwood <kozimoto@gmail.com>
2015-06-02 17:03:14 +1000

    Updated as of end 2 june 2015
    -changed column names of processed beta and binary meth sets
    -added some CIMP ranking
    -added some statistical analysis

--- Methylation_sets.R
+++ Methylation_sets.R
@@ -45,13 +45,33 @@ hist(Fang_Ave$`rowMeans(CentreFang)`)
 ##Do averages of the binary
 
 Fang_Collect <- as.data.frame(rowMeans(fang_bin))
-
-hist(Fang_Collect$`rowMeans(fang_bin)`)
+colnames(Fang_Collect)[1] <- "MethBin"
+hist(Fang_Collect$MethBin)
 
 
 ##########################################
 
-### Seperate into low and high methylation groups by quartile
+## Only the top 3 Fang CIMP genes
+B_cimp <- data.frame(fang_bin$ALX4)
+B_cimp$ARHGEF7 <- fang_bin$ARHGEF7
+B_cimp$RASGRF2 <- fang_bin$RASGRF2
+
+colnames(B_cimp)[1] <- "ALX4"
+
+### Get a sum for the 3 genes methylation status
+B_cimp$Total <- rowSums(B_cimp) 
+## Set the cut off (2/3 genes methylated = B_CIMP+)
+B_cimp$ME_Rank[B_cimp$Total>=2] <-"Pos"
+B_cimp$ME_Rank[B_cimp$Total<=1]<- "Neg"
+
+count(B_cimp$ME_Rank)
+##Most are neg??
+
+##set as a factor
+
+CIMP <- as.factor(B_cimp$ME_Rank)
+
+### Seperate into low and high methylation groups by quartile - beta group
 colnames(Fang_Ave)[1] <- "MethScore"
 attach(Fang_Ave)
 ### for 4 quartiles
@@ -79,10 +99,36 @@ count(Values$half)
 #### Set the MethScore quartiles as a factor
 
 Quar <- as.factor(Values$quartile)
+Half <- as.factor(Values$half)
 
 #### Look at expression of DNMT by quartile
+### Do a box plot, anova, etc
 
-boxplot(exp$DNMT1)
+boxplot(exp$DNMT1 ~ Quar)
+boxplot(exp$DNMT1 ~ Half)
+boxplot(exp$DNMT1 ~ CIMP)
+
+boxplot(exp$DNMT3A ~ Quar)
+
+boxplot(exp$DNMT3B ~ Quar)
+
+### Survivial curves
+
+fit.diff = survdiff(Surv(OS_Time_nature2012,OS_event_nature2012 == 1) ~ factor(CIMP),
+                    data=clin) 
+chisq2 = signif(1-pchisq(fit.diff$chisq,length(levels(factor(CIMP)))-1),3) 
+fit1 = survfit(Surv(OS_Time_nature2012,OS_event_nature2012 == 1)~CIMP,
+               data=clin,conf.type="log-log") 
+kmcolours <- c("black", "red", "green", "blue")
+plot(fit1, conf.int=F,col=kmcolours,xlab="Time to death (days)", 
+     ylab="Survival",main=c("All subtype survival by\n methylation CIMPtile "), 
+     lwd=4,mark.time=TRUE)
+legend("bottomleft",legend=levels(factor(CIMP)),
+       fill = kmcolours, cex = 1)
+
+###do another anova for signifigance
+anova(coxph(Surv(OS_Time_nature2012,OS_event_nature2012 == 1)~factor(CIMP), data = clin))
+
 
 
 
@@ -93,3 +139,107 @@ stir_beta <- read.table("Stir_beta.txt", sep = "\t", header = TRUE, row.names = 
 
 stir_bin <- read.table("Stir_binary.txt", sep = "\t", header = TRUE, row.names = 1)
 
+### Do mean centred averages of the beta ############################
+
+# center with 'colMeans()'
+center_colmeans <- function(x) {
+  xcenter = colMeans(x)
+  x - rep(xcenter, rep.int(nrow(x), ncol(x)))
+}
+
+# apply it
+CentreStir <- center_colmeans(stir_beta)
+
+Stir_Ave <- as.data.frame(rowMeans(CentreStir))
+
+rownames(Stir_Ave) <- rownames(stir_beta) 
+colnames(Stir_Ave)[1] <- "MethScore"
+
+write.table(Stir_Ave, "Stir_beta_average.txt", sep = "\t")
+
+hist(Stir_Ave$MethScore)
+
+#########################################
+
+##Do averages of the binary
+
+Stir_Collect <- as.data.frame(rowMeans(stir_bin))
+colnames(Stir_Collect)[1] <- "MethBin"
+
+hist(Stir_Collect$MethBin)
+
+
+#######################
+attach(Stir_Ave)
+### for 4 quartiles
+
+brks <- with(Stir_Ave, quantile(MethScore, probs = c(0, 0.25, 0.5, 0.75, 1)))
+Values <- within(Stir_Ave, quartile <- cut(MethScore, breaks = brks, labels = 1:4, 
+                                           include.lowest = TRUE))
+
+
+library(plyr) ## for the "count" function, which is sweeeet!
+count(Values$quartile)
+## you can see that the distribution is very even / same kinda numbers in each quartile!
+
+### for only high vs low
+
+brks <- with(Stir_Ave, quantile(MethScore, probs = c(0, 0.5, 1)))
+Values <- within(Stir_Ave, half <- cut(MethScore, breaks = brks, labels = 1:2, 
+                                       include.lowest = TRUE))
+
+count(Values$half)
+
+
+#### and you're off and running!!
+
+#### Set the MethScore quartiles as a factor
+
+Quar <- as.factor(Values$quartile)
+Half <- as.factor(Values$half)
+
+boxplot(exp$DNMT1 ~ Half)
+
+### Survivial curves
+
+fit.diff = survdiff(Surv(OS_Time_nature2012,OS_event_nature2012 == 1) ~ factor(Half),
+                    data=clin) 
+chisq2 = signif(1-pchisq(fit.diff$chisq,length(levels(factor(Half)))-1),3) 
+fit1 = survfit(Surv(OS_Time_nature2012,OS_event_nature2012 == 1)~Half,
+               data=clin,conf.type="log-log") 
+kmcolours <- c("black", "red", "green", "blue")
+plot(fit1, conf.int=F,col=kmcolours,xlab="Time to death (days)", 
+     ylab="Survival",main=c("All subtype survival by\n methylation Halftile "), 
+     lwd=4,mark.time=TRUE)
+legend("bottomleft",legend=levels(factor(Half)),
+       fill = kmcolours, cex = 1)
+
+###do another anova for signifigance
+anova(coxph(Surv(OS_Time_nature2012,OS_event_nature2012 == 1)~factor(Quar), data = clin))
+
+
+
+
+############## Methylation by subtype
+
+Pam50 <- factor(clin$PAM50Call_RNAseq) 
+ER <- factor(clin$ER_Status_nature2012)
+PR <- factor(clin$PR_Status_nature2012)
+Her <- factor(clin$HER2_Final_Status_nature2012)
+Mets <- factor(clin$Metastasis_Coded_nature2012)
+Node <- factor(clin$Node_Coded_nature2012)
+
+
+stripchart(Fang_Ave$MethScore ~ Pam50)
+stripchart(Stir_Ave$MethScore ~ Pam50)
+
+stripchart(Fang_Collect$MethBin ~ Pam50)
+stripchart(Stir_Collect$MethBin ~ Pam50)
+
+stripchart(Fang_Ave$MethScore ~ ER)
+stripchart(Stir_Ave$MethScore ~ Her)
+
+
+boxplot(exp$DNMT1 ~ Pam50)
+boxplot(Fang_Ave$MethScore ~ Pam50)
+boxplot(Stir_Ave$MethScore ~ Pam50)
